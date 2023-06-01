d7f0fdfd4551cb01f94109f28a2c100ccc72a317
TheoCrd <theo38.couard@hotmail.fr>
2023-06-01 18:11:49 +0200

    🐛 [Fix] limit boids to the currents boundaries

--- src/App/AppUpdate.cpp
+++ src/App/AppUpdate.cpp
@@ -65,6 +65,7 @@ inline void App::updateBoids()
 {
     for (auto& boid : boids)
     {
+        boid.avoidEdges(boid);
         boid.flock(boids, m_ctx, m_intensities); // updating the boids here
     }
 }
